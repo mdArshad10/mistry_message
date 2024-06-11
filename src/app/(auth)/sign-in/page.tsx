a0919b6166153b3b2025ae89544f4f31c5938a87
mdArsahd10 <mdarshd246@gmail.com>
2024-06-11 16:15:32 +0530

    add the sign-in in frontend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,19 @@
 "use client";
-const page = () => {
-  return <div>This is the page</div>;
-};
-
-export default page;
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button className="px-3 py-1 rounded-2xl bg-orange-400" onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
